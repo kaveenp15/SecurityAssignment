5d32c39e2966d64a1bb33fd7c6447342eaf3edc0
kaveenp15 <87686843+kaveenp15@users.noreply.github.com>
2021-08-08 15:00:19 +0530

    Create LT-2021-043q6.py

---
+++ LT-2021-043q6.py
@@ -0,0 +1,7 @@
+import hashlib
+import sys
+
+value = (sys.argv[1])
+hashValue = hashlib.pbkdf2_hmac('sha512', value.encode(), b'Km5d5ivMy8iexuHcZrsD', 200000)
+hexvalue = hashValue.hex()
+print(hexvalue)
