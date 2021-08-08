e35ed802fe79dd5cd2d452f7fcb238bd699e5c76
kaveenp15 <87686843+kaveenp15@users.noreply.github.com>
2021-08-08 14:48:54 +0530

    Create LT-2021-043q7.py

---
+++ LT-2021-043q7.py
@@ -0,0 +1,11 @@
+import hashlib
+import sys
+import random
+import string
+def my_string(v = 10):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(v))
+salt = my_string()
+value = (sys.argv[1])
+hashValue = hashlib.pbkdf2_hmac('sha512', value.encode(), salt.encode(), 200000)
+hexvalue = hashValue.hex()
+print(hexvalue)
