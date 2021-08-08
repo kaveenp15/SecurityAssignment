19525c819277a659a19e7de25940f7d3a67ac99c
kaveenp15 <87686843+kaveenp15@users.noreply.github.com>
2021-08-08 14:59:11 +0530

    Delete Q6script.py

--- Q6script.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-import sys
-
-value = (sys.argv[1])
-hashValue = hashlib.pbkdf2_hmac('sha512', inValue.encode(), b'Km5d5ivMy8iexuHcZrsD', 200000)
-hexvalue = hashValue.hex()
-print(hexvalue)
